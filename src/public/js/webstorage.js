95983b051c2acb813771dc529c07558b45a9c3ce
Eric <rc84.eric@gmail.com>
2020-10-29 17:31:48 +0100

    user & email localstorage

---
+++ src/public/js/webstorage.js
@@ -0,0 +1,27 @@
+// NOMBRE & EMAIL
+
+function obtener_claves_local() {
+    if (window.localStorage != "undefined") {
+        //Recuperar claves y escribirlas en los input correspondientes
+        var usuario = localStorage.getItem("usuario");
+        $('#login_username').val(usuario);
+        var email = localStorage["email"];
+        $('#login_email').val(email);
+    }
+}
+
+function guardar_claves_local() {
+    if (window.localStorage != "undefined") {
+        //Guardar claves con los valores de los inputs
+        var usuario = $('#login_username').val();
+        localStorage.setItem("usuario", usuario);
+        var email = $('##login_email').val();
+        localStorage.setItem("email", email);
+    }
+}
+
+function local() {
+    $('#guardar_claves_local').on("click", function() {
+        guardar_claves_local();
+    });
+}

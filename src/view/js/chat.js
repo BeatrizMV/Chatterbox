d1e097dbdcaefb07c48fa57b4960487eab027162
Erik <erikpastorrios1994@gmail.com>
2020-11-29 17:14:30 +0100

    Change names to be more clear and delete console.log

--- src/view/js/chat.js
+++ src/view/js/chat.js
@@ -1,41 +1,41 @@
 var socket;
 
 function sendMessage(event) {
-    //event.preventDefault();
-    const msg = document.getElementById("message").value;
-    console.log("Sending message to the chat: " + msg);
-    document.getElementById("message").value = "";
-    socket.emit('message', msg);
-    createMsgElemInScreen(msg, true);
+  // event.preventDefault();
+  const message = {
+    message: document.getElementById("message").value,
+    user: localStorage.getItem("email"),
+  };
+  document.getElementById("message").value = "";
+  socket.emit("message", message);
+  addMessageToScreen(message, true);
 }
 
+// eslint-disable-next-line no-unused-vars
 function submitMessage(event) {
-    console.log("from SUBMIT");
-    sendMessage(event);
+  sendMessage(event);
 }
 
-function createMsgElemInScreen (msg, isMine){
-    //cogemos el contenedor de los mensajes
-    const messages = document.getElementById("messages-list");
-    //creamos el list item para el mensaje
-    const listItem = document.createElement("LI");
-    const text = document.createTextNode(msg);
-    if(isMine) {
-        listItem.className = "message_sent--mine";
-    } else {
-        listItem.className = "message_sent";
-    }
-    listItem.appendChild(text);
-    messages.appendChild(listItem);
+function addMessageToScreen(message, isMine) {
+  // cogemos el contenedor de los mensajes
+  const messages = document.getElementById("messages-list");
+  // creamos el list item para el mensaje
+  const listItem = document.createElement("LI");
+  const text = document.createTextNode(message.message);
+  if (isMine) {
+    listItem.className = "message_sent--mine";
+  } else {
+    listItem.className = "message_sent";
+  }
+  listItem.appendChild(text);
+  messages.appendChild(listItem);
 }
 
 $(document).ready(function () {
-    console.log("after page loaded");
-    socket = io.connect("http://localhost:8000");
+  // eslint-disable-next-line no-undef
+  socket = io.connect("http://localhost:8000");
 
-    socket.on('message', function(msg){
-        console.log("Received message from socket: " + msg);
-        createMsgElemInScreen(msg, false);
-    });
-
-})
+  socket.on("message", function (message) {
+    addMessageToScreen(message, false);
+  });
+});

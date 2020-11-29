613afc72f531ee7170e615abe08a60f5503f0190
Erik <erikpastorrios1994@gmail.com>
2020-11-29 09:42:16 +0100

    Fix socket.io server

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ const path = require("path");
 
 const config = require("./config");
 
-const router = require("./router")
-
-const WS_PORT = 8000;
+const router = require("./router");
 
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, "./public")));
@@ -21,21 +19,14 @@ app.listen(config.port, config.hostname, () => {
   console.log(`Server listening on http://${config.hostname}:${config.port}`); // eslint-disable-line no-console
 });
 
+var http = require("http");
 
-//Socket setup
-var http = require('http');
+const server = http
+  .createServer()
+  .listen(config.webservicePort, config.hostname);
 
-const server = http.createServer(function (req, res) {
-//nothing here
-}).listen(WS_PORT);
+const io = socket(server, { cors: { origin: "*" } });
 
-
-//const server = app.listen(WS_PORT, function (){
-//  console.log("Listening ws on port: " + WS_PORT);
-//});
-
-const io = socket(server);
-
-io.on('connection', socket => {
+io.on("connection", () => {
   console.log("Nuevo usuario conectado");
-})
+});
